fe461b600ac48c8c0af364b3632257d01315d7ac
aragan <64033062+aragan@users.noreply.github.com>
2022-02-27 16:03:23 +0300

    Add files via upload

--- WAR.lua
+++ WAR.lua
@@ -114,7 +114,7 @@ function init_gear_sets()
      sets.precast.JA['Blood Rage'] = { body="Boii Lorica +1" }
      sets.precast.JA['Provoke'] = set_combine(sets.TreasureHunter, { hands="Pummeler's Mufflers +1"})
      sets.precast.JA['Berserk'] = { body="Pummeler's Lorica +3", hands="Agoge Calligae", back=Cichols.TP, feet="Agoge Calligae"}
-     sets.precast.JA['Warcry'] = { head={ name="Agoge Mask +3", augments={'Enhances "Savagery" effect',}},
+     sets.precast.JA['Warcry'] = { head={ name="Agoge Mask +3", augments={'Enhances "Savagery" effect',}},}
      sets.precast.JA['Mighty Strikes'] = { head="Agoge Mufflers"}
      sets.precast.JA['Retaliation'] = { hands="Pummeler's Mufflers +1", feet="Ravager's Calligae +2"}
      sets.precast.JA['Aggressor'] = { head="Pummeler's Mask +1", body="Agoge Lorica"}
@@ -260,19 +260,41 @@ function init_gear_sets()
      -- RESOLUTION
      -- 86-100% STR
      sets.precast.WS['Resolution'] = set_combine(sets.precast.WS, {
-         head="Hjarrandi Helm",
+        sub="Utu Grip",
+        ammo={ name="Seeth. Bomblet +1", augments={'Path: A',}},
+        head="Sakpata's Helm",
+        body={ name="Tatena. Harama. +1", augments={'Path: A',}},
+        hands="Sakpata's Gauntlets",
+        legs={ name="Tatena. Haidate +1", augments={'Path: A',}},
          neck="Fotia Gorget",
-         ear1="Schere Earring",
+         left_ring="Niqmaddu Ring",
+         right_ring="Shukuyu Ring",
+         left_ear={ name="Moonshade Earring", augments={'Accuracy+4','TP Bonus +250',}},
+         right_ear="Thrud Earring",
          hands="Sakpata's Gauntlets",
          legs="Sakpata's Cuisses",
     	 body="Sakpata's Plate'",
          waist="Fotia Belt",
          feet="Flamma Gambieras +2"
+         back={ name="Cichol's Mantle", augments={'STR+20','Accuracy+20 Attack+20','STR+10','Weapon skill damage +10%','Phys. dmg. taken-10%',}},
+
      })
      sets.precast.WS['Resolution'].Mid = set_combine(sets.precast.WS.Resolution, {
-         head="Flamma Zucchetto +2",
-         ammo="Coiste Bodhar",
-         --head="Valorous Mask",
+        sub="Utu Grip",
+        ammo={ name="Seeth. Bomblet +1", augments={'Path: A',}},
+        head="Sakpata's Helm",
+        body="Sakpata's Plate",
+        hands="Sakpata's Gauntlets",
+        legs="Sakpata's Cuisses",
+        feet="Sakpata's Leggings",
+         neck="Fotia Gorget",
+         left_ring="Niqmaddu Ring",
+         right_ring="Shukuyu Ring",
+         left_ear={ name="Moonshade Earring", augments={'Accuracy+4','TP Bonus +250',}},
+         right_ear="Thrud Earring",
+         waist="Fotia Belt",    
+         back={ name="Cichol's Mantle", augments={'STR+20','Accuracy+20 Attack+20','STR+10','Weapon skill damage +10%','Phys. dmg. taken-10%',}},
+
      })
      sets.precast.WS['Resolution'].Acc = set_combine(sets.precast.WS.Resolution.Mid, sets.precast.WS.Acc) 
 
@@ -298,7 +320,7 @@ function init_gear_sets()
     sets.precast.WS['Impulse Drive'] = set_combine(sets.precast.WS, {
         
     ammo="Knobkierrie",
-    head="Sakpata's Helm",
+    head={ name="Agoge Mask +3", augments={'Enhances "Savagery" effect',}},
     body="Sakpata's Plate",
     hands="Sakpata's Gauntlets",
     legs="Sakpata's Cuisses",
@@ -397,7 +419,7 @@ function init_gear_sets()
      })
      sets.idle.Regen = set_combine(sets.idle.Field, {
          ear2="Infused Earring",
-         hands="Volte Moufles",
+         
          ring1="Paguroidea Ring",
      })
  
@@ -475,7 +497,7 @@ function init_gear_sets()
          hands="Sakpata's Gauntlets",
          ring1="Niqmaddu Ring",
          ring2="Flamma Ring",
-         waist="Ioskeha Belt",
+         waist="Ioskeha Belt +1",
          legs="Tatenashi Haidate +1",
          feet="Tatenashi Sune-ate +1"
     	 --body="Flamma Korazin +2"
@@ -487,7 +509,7 @@ function init_gear_sets()
         hands={ name="Tatena. Gote +1", augments={'Path: A',}},
         legs={ name="Tatena. Haidate +1", augments={'Path: A',}},
         feet={ name="Tatena. Sune. +1", augments={'Path: A',}},
-        neck={ name="Vim Torque +1", augments={'Path: A',}},
+        neck={ name="War. Beads +2", augments={'Path: A',}},
         waist="Ioskeha Belt +1",
         left_ear="Cessance Earring",
         right_ear="Telos Earring",
@@ -501,17 +523,26 @@ function init_gear_sets()
      sets.engaged.Acc.PDT = set_combine(sets.engaged.Acc, sets.Defensive_Acc)
 
      sets.engaged.DW = set_combine(sets.engaged, {
-        ear1="Eabani Earring",
-        ear2="Suppanomimi",
-        hands="Emicho Gauntlets",
-        waist="Shetal Stone"
+        ammo="Coiste Bodhar",
+    head="Sakpata's Helm",
+    body="Sakpata's Plate",
+    hands="Sakpata's Gauntlets",
+    legs={ name="Tatena. Haidate +1", augments={'Path: A',}},
+    feet={ name="Tatena. Sune. +1", augments={'Path: A',}},
+    neck={ name="War. Beads +2", augments={'Path: A',}},
+    waist="Ioskeha Belt +1",
+    left_ear="Eabani Earring",
+    right_ear="Schere Earring",
+    left_ring="Niqmaddu Ring",
+    right_ring="Petrov Ring",
+    back="Atheling Mantle",
      })
      sets.engaged.OneHand = set_combine(sets.engaged, {
          head="Hjarrandi Helm",
     	 body="Hjarrandi Breastplate", 
          ear1="Telos Earring",
          ear2="Cessance Earring",
-         waist="Ioskeha Belt",
+         waist="Ioskeha Belt +1",
          legs="Tatenashi Haidate +1",
          feet="Tatenashi Sune-ate +1",
          --ring2="Hetairoi Ring"
