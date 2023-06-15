3cf752fcc692441f5228c74758d74ada9b0d0fbd
aragan <64033062+aragan@users.noreply.github.com>
2023-06-16 02:38:36 +0300

    Add files via upload

--- PLD.lua
+++ PLD.lua
@@ -15,6 +15,7 @@ function get_sets()
     include('Mote-IncludePLD.lua')
     include('organizer-lib')
     organizer_items = {
+        "Foreshock Sword",
         "Mafic Cudgel",
         "Gyudon",
         "Reraiser",
