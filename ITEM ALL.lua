7572232ca02d0c3cfcf90b832c306398aab4b3f7
aragan <64033062+aragan@users.noreply.github.com>
2023-10-11 23:31:48 +0300

    Add files via upload

---
+++ ITEM ALL.lua
@@ -0,0 +1,51 @@
+new item
+
+    "Tumult's Blood",
+    "Sarama's Hide",
+    "Hidhaegg's Scale",
+    "Sovereign's Hide",
+    "Grape Daifuku",
+    "Soy Ramen",
+    "G. Curry Bun +1",
+    "Pukatrice Egg",
+    "Moogle Amp.",
+    "Popo. con Queso",
+    "Pear Crepe",
+    "Crab Sushi",
+    "Om. Sandwich",
+    "Red Curry Bun",
+
+
+head="Nyame Helm",
+body="Nyame Mail",
+hands="Nyame Gauntlets",
+legs="Nyame Flanchard",
+feet="Nyame Sollerets",
+
+head="Sakpata's Helm",
+body="Sakpata's Plate",
+hands="Sakpata's Gauntlets",
+legs="Sakpata's Cuisses",
+feet="Sakpata's Leggings",
+
+right_ring="Sroda Ring",
+ammo="Crepuscular Pebble",
+
+
+body="Councilor's Garb",
+right_ring={ name="Metamor. Ring +1", augments={'Path: A',}},
+
+back="Sacro Mantle",
+
+feet={ name="Hippo. Socks +1", augments={'Path: A',}},
+
+
+--DW    EQUIPE
+
+body={ name="Adhemar Jacket +1", augments={'DEX+12','AGI+12','Accuracy+20',}},
+hands="Floral Gauntlets",
+legs={ name="Carmine Cuisses +1", augments={'Accuracy+20','Attack+12','"Dual Wield"+6',}},
+feet={ name="Taeon Boots", augments={'Pet: Attack+25 Pet: Rng.Atk.+25','Pet: "Dbl. Atk."+5','Pet: Damage taken -4%',}},
+waist="Reiki Yotai",
+left_ear="Suppanomimi",
+right_ear="Eabani Earring",
