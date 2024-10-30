d0a6e9dbdee9540048cc21a8cb8b303874f2b2f2
aragan <64033062+aragan@users.noreply.github.com>
2024-10-30 22:12:20 +0300

    Add files via upload

--- BLU.lua
+++ BLU.lua
@@ -406,7 +406,6 @@ function init_gear_sets()
         back="Reiki Cloak",
         }
     sets.precast.JA['Sublimation'] = {
-        waist="Embla Sash",
     }
     sets.precast.JA['Provoke'] = sets.Enmity
     -- Precast sets to enhance JAs
@@ -1759,6 +1758,26 @@ function job_pretarget(spell, action, spellMap, eventArgs)
     if spell.type:endswith('Magic') and buffactive.silence then
         eventArgs.cancel = true
         send_command('input /item "Remedy" <me>')
+    end
+    if spell.english == 'Searing Tempest' or spell.english == 'Spectral Floe' 
+	or spell.english == 'Anvil Lightning' or spell.english == 'Scouring Spate' 
+    or spell.english == 'Nectarous Deluge' or spell.english == 'Rending Deluge' 
+	or spell.english == 'Droning Whirlwind' or spell.english == 'Gates of Hades' 
+	or spell.english == 'Thunderbolt' or spell.english == 'Rail Cannon' or spell.english == 'Atra. Libations' 
+	or spell.english == 'Entomb' or spell.english == 'Tenebral Crush' 
+	or spell.english == 'Palling Salvo' or spell.english == 'Blinding Fulgor' then
+
+        local allRecasts = windower.ffxi.get_ability_recasts()
+        local Burst_AffinityCooldown = allRecasts[182]
+        
+        
+        if player.main_job_level >= 25 and Burst_AffinityCooldown < 3 then
+            cast_delay(1.1)
+            send_command('@input /ja "Burst Affinity" <me>')
+        end
+        if not midaction() then
+            job_update()
+        end
     end
 end
 function job_precast(spell, action, spellMap, eventArgs)
